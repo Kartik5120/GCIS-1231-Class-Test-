11bae400504bd1dfb2b9b3636c9c8d11fb298ae6
Kartik5120 <kr5120@g.rit.edu>
2022-01-12 14:02:48 +0400

    Fixed the line spacing as well as fixed the currency issue

--- test-1.py
+++ test-1.py
@@ -4,10 +4,11 @@ print("\n","Also, hello there wolf bane!")
 
 print("\n","Wolfbane, do you have any currency on you?")
 
-currency = int(input("How much currency do you have?[Please enter the number only]"))
+print("")
 
-def wolfbane():
-    if currency >= 10:
-        print ("You are not welcome here and kindly leave the city of the dead!!!")
-    elif currency <= 10: 
-        print ("Here is the currency of this world")
+currency = int(input("How much currency do you have?[Please enter the number only]: "))
+
+if currency >= 10:
+    print ("\n","You are not welcome here and kindly leave the city of the dead!!!")
+else:
+    print ("\n", "Here is the currency of this world")
