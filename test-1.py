cd4226f7e90da365556d7359112dd1d8a3eb6e84
Kartik5120 <kr5120@g.rit.edu>
2022-01-12 13:57:23 +0400

    Have written all of the introduction lines, requires a fix in def of wolfbane asking currency

--- test-1.py
+++ test-1.py
@@ -1,3 +1,13 @@
 print("Hello World!!")
 
 print("\n","Also, hello there wolf bane!")
+
+print("\n","Wolfbane, do you have any currency on you?")
+
+currency = int(input("How much currency do you have?[Please enter the number only]"))
+
+def wolfbane():
+    if currency >= 10:
+        print ("You are not welcome here and kindly leave the city of the dead!!!")
+    elif currency <= 10: 
+        print ("Here is the currency of this world")
